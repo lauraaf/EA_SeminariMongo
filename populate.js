da9132f0ac1bf8a4c46a9d1e516bc47fd428ae1f
lauraaf <laura.fernandez.garcia@estudiantat.upc.edu>
2024-10-01 13:50:59 +0200

    Versió 5

--- populate.js
+++ populate.js
@@ -4,30 +4,11 @@ require('./connection');
 const Author = require('./models/Author');
 const Song = require('./models/Song');
 
-// Crear canciones y asociarlas con el autor
-const createSongs = async () => {
-    const author = new Author({
-        name: 'Nina Simone',
-        audienceCount: 45
-    });
-    await author.save();
-    
-    const song1 = new Song({
-        name: 'My baby just cares for me',
-        author: author._id,
-        album: 'Little girl blue',
-        duration: 3.34
-    });
-    await song1.save();
-};
-
-
-
 // Obtener el autor de una canción
 const getAuthor = async () => {
     const song = await Song
-        .findOne({ name: 'My baby just cares for me' })
-        .populate('author');  // Asegúrate de que 'author' esté definido correctamente en tu esquema
+        .findOne({ name: 'I dont want to miss a thing' })
+        .populate('author');  
     if (song && song.author) {
         console.log(`The author is ${song.author.name}`);
     } else {
@@ -35,4 +16,4 @@ const getAuthor = async () => {
     }
 };
 
-createSongs().then(getAuthor);
+getAuthor();
